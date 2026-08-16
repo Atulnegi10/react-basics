09942b7230be67c59f3a7788f13b2785a94e377a
Atulnegi10 <natul8514@gmail.com>
2026-08-16 20:30:48 +0530

    created first react project

---
+++ first.jsx
@@ -0,0 +1,24 @@
+import {useSTATE} from 'react';
+import "./app.css";
+
+export default function App(){
+  const [count, setcount] = useSTATE(0);
+
+  function onClickHandler(){
+    setcount(count+1);
+  }
+  return(
+    <div>
+    <button onClickHandler = {onClickHandler}>
+    </button>
+    </div>
+  );
+}
+
+function button(props){
+  return (
+    <button onClick={props.onClickHandler}>
+      counter{props.count}
+    </button>
+  );
+}
